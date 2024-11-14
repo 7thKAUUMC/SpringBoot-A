2633fa31150d18b7c3c479fd54a43b8e0e45afab
jungmin <dbswjdals2580@gmail.com>
2024-11-15 01:37:16 +0900

    "mission 6 : 하는중..."

---
+++ src/main/java/umc/study/convert/TempConverter.java
@@ -0,0 +1,12 @@
+package umc.study.convert;
+
+import umc.study.web.dto.TempResponse;
+
+public class TempConverter {
+
+    public static TempResponse.TempTestDTO toTempTestDTO(){
+        return TempResponse.TempTestDTO.builder()
+                .testString("This is Test!")
+                .build();
+    }
+}
