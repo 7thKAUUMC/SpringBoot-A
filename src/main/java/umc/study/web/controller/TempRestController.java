ecb4675401e01ddfc779e1eaed5456c1d98f00d9
jungmin <dbswjdals2580@gmail.com>
2024-11-15 02:25:20 +0900

    "mission 7 : 미션성공!"

--- src/main/java/umc/study/web/controller/TempRestController.java
+++ src/main/java/umc/study/web/controller/TempRestController.java
@@ -3,9 +3,11 @@ package umc.study.web.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.study.apiPayload.ApiResponse;
 import umc.study.convert.TempConverter;
+import umc.study.service.TempService.TempQueryService;
 import umc.study.web.dto.TempResponse;
 
 @RestController
@@ -17,5 +19,11 @@ public class TempRestController {
     public ApiResponse<TempResponse.TempTestDTO> testAPI(){
 
         return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+
+    @GetMapping("/exception")
+    public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam Integer flag){
+
+        return null;
     }
 }
